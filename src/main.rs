e70a2fe8510f47a46a0050db54df54914772ec11
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-01-19 11:46:30 +0100

    add north screen edge bump

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use std::thread::sleep_ms;
+
 use swayipc::{Connection, Error, Fallible, Node, NodeType};
 
 fn main() -> Fallible<()> {
     let mut ipc = Connection::new()?;
-    let focused_node =
-        find_focused_node(ipc.get_tree()?.nodes, ipc.get_tree()?.floating_nodes)?.unwrap();
-    println!("{:#?}", focused_node.node_type);
-    float_node(&mut ipc)?;
-    move_window(&mut ipc, Direction::NW);
-    println!("{:#?}", focused_node.node_type);
-    Ok(())
+    // move_window(&mut ipc, Direction::NW);
+    let mut direction = Direction::NE;
+    loop {
+        direction = get_next_direction(&mut ipc, direction)?;
+        println!("{:#?}", direction);
+        move_window(&mut ipc, &direction);
+        sleep_ms(100);
+    }
 }
 
 fn find_focused_node(nodes: Vec<Node>, floating_nodes: Vec<Node>) -> Fallible<Option<Node>> {
@@ -17,6 +20,7 @@ fn find_focused_node(nodes: Vec<Node>, floating_nodes: Vec<Node>) -> Fallible<Op
     for node in all_nodes {
         if node.focused && node.node_type != NodeType::Workspace {
             println!("{}", node.name.as_ref().unwrap());
+            println!("{:#?}", node.rect);
             return Ok(Some(node));
         }
         match find_focused_node(node.nodes, node.floating_nodes)? {
@@ -27,20 +31,33 @@ fn find_focused_node(nodes: Vec<Node>, floating_nodes: Vec<Node>) -> Fallible<Op
     Ok(None)
 }
 
+#[derive(Debug, PartialEq)]
 enum Direction {
     NE,
-    ES,
+    SE,
     SW,
     NW,
 }
 
-fn move_window(ipc: &mut Connection, direction: Direction) {
+fn get_next_direction(ipc: &mut Connection, old_direction: Direction) -> Fallible<Direction> {
+    let focused_node =
+        find_focused_node(ipc.get_tree()?.nodes, ipc.get_tree()?.floating_nodes)?.unwrap();
+    let display_dimensions = ipc.get_tree()?.rect;
+    println!("AAAAAAAAAA{:?}", focused_node.rect);
+    if focused_node.rect.y <= 0 && old_direction == Direction::NE {
+        return Ok(Direction::SE);
+    }
+    // println!("{:#?}", display_dimensions);
+    Ok(old_direction)
+}
+
+fn move_window(ipc: &mut Connection, direction: &Direction) {
     match direction {
         Direction::NE => {
             _ = ipc.run_command(format!("move up {}", 1));
             _ = ipc.run_command(format!("move right {}", 1));
         }
-        Direction::ES => {
+        Direction::SE => {
             _ = ipc.run_command(format!("move up {}", -1));
             _ = ipc.run_command(format!("move right {}", 1));
         }
