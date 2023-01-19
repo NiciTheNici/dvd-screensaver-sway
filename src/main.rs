485dfc89c004e225c6b4ca324e849c1c56763cd4
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-01-19 14:32:07 +0100

    add corner bounce to all 4 directions

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,11 @@ use swayipc::{Connection, Error, Fallible, Node, NodeType};
 fn main() -> Fallible<()> {
     let mut ipc = Connection::new()?;
     // move_window(&mut ipc, Direction::NW);
-    let mut direction = Direction::NE;
+    let mut direction = Direction::NW;
     loop {
         direction = get_next_direction(&mut ipc, direction)?;
-        println!("{:#?}", direction);
         move_window(&mut ipc, &direction);
-        sleep_ms(100);
+        // sleep_ms(10);
     }
 }
 
@@ -19,8 +18,6 @@ fn find_focused_node(nodes: Vec<Node>, floating_nodes: Vec<Node>) -> Fallible<Op
 
     for node in all_nodes {
         if node.focused && node.node_type != NodeType::Workspace {
-            println!("{}", node.name.as_ref().unwrap());
-            println!("{:#?}", node.rect);
             return Ok(Some(node));
         }
         match find_focused_node(node.nodes, node.floating_nodes)? {
@@ -43,10 +40,50 @@ fn get_next_direction(ipc: &mut Connection, old_direction: Direction) -> Fallibl
     let focused_node =
         find_focused_node(ipc.get_tree()?.nodes, ipc.get_tree()?.floating_nodes)?.unwrap();
     let display_dimensions = ipc.get_tree()?.rect;
-    println!("AAAAAAAAAA{:?}", focused_node.rect);
-    if focused_node.rect.y <= 0 && old_direction == Direction::NE {
-        return Ok(Direction::SE);
+    println!("Display {:#?}", display_dimensions);
+    println!("Focus {:#?}", focused_node.rect);
+
+    match old_direction {
+        Direction::NE => {
+            if focused_node.rect.x + focused_node.rect.width >= display_dimensions.width {
+                return Ok(Direction::NW);
+            }
+
+            if focused_node.rect.y <= 0 {
+                return Ok(Direction::SE);
+            }
+        }
+        Direction::SE => {
+            if focused_node.rect.x + focused_node.rect.width >= display_dimensions.width {
+                return Ok(Direction::SW);
+            }
+
+            if focused_node.rect.y + focused_node.rect.height >= display_dimensions.height {
+                return Ok(Direction::NE);
+            }
+        }
+        Direction::SW => {
+            if focused_node.rect.x <= 0 {
+                return Ok(Direction::SE);
+            }
+
+            if focused_node.rect.y + focused_node.rect.height >= display_dimensions.height {
+                return Ok(Direction::NW);
+            }
+        }
+        Direction::NW => {
+            if focused_node.rect.x <= 0 {
+                return Ok(Direction::NE);
+            }
+
+            if focused_node.rect.y <= 0 {
+                return Ok(Direction::SW);
+            }
+        }
     }
+
+    if old_direction == Direction::NE {}
+
     // println!("{:#?}", display_dimensions);
     Ok(old_direction)
 }
