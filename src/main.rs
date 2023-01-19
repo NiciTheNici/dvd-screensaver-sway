ec802e1e26f9cb2ecf15666c0fc952c107279f42
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-01-19 16:42:51 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use std::thread::sleep_ms;
-
+use std::{thread, time};
 use swayipc::{Connection, Error, Fallible, Node, NodeType};
+
+#[derive(Debug, PartialEq)]
+enum Direction {
+    NE,
+    SE,
+    SW,
+    NW,
+}
 
 fn main() -> Fallible<()> {
     let mut ipc = Connection::new()?;
-    // move_window(&mut ipc, Direction::NW);
     let mut direction = Direction::NW;
+
+    ipc.run_command("floating on")?;
     loop {
         direction = get_next_direction(&mut ipc, direction)?;
-        move_window(&mut ipc, &direction);
-        // sleep_ms(10);
+        move_focused_window(&mut ipc, &direction);
+        thread::sleep(time::Duration::from_millis(10));
     }
 }
 
@@ -25,70 +33,64 @@ fn find_focused_node(nodes: Vec<Node>, floating_nodes: Vec<Node>) -> Fallible<Op
             None => (),
         };
     }
+
     Ok(None)
 }
 
-#[derive(Debug, PartialEq)]
-enum Direction {
-    NE,
-    SE,
-    SW,
-    NW,
-}
-
 fn get_next_direction(ipc: &mut Connection, old_direction: Direction) -> Fallible<Direction> {
-    let focused_node =
-        find_focused_node(ipc.get_tree()?.nodes, ipc.get_tree()?.floating_nodes)?.unwrap();
+    let focused_node: Node =
+        match find_focused_node(ipc.get_tree()?.nodes, ipc.get_tree()?.floating_nodes)? {
+            Some(r) => r,
+            None => {
+                return Err(Error::CommandFailed(
+                    "Could not find focused node".to_string(),
+                ))
+            }
+        };
     let display_dimensions = ipc.get_tree()?.rect;
-    println!("Display {:#?}", display_dimensions);
-    println!("Focus {:#?}", focused_node.rect);
 
     match old_direction {
         Direction::NE => {
             if focused_node.rect.x + focused_node.rect.width >= display_dimensions.width {
                 return Ok(Direction::NW);
             }
-
             if focused_node.rect.y <= 0 {
                 return Ok(Direction::SE);
             }
         }
+
         Direction::SE => {
             if focused_node.rect.x + focused_node.rect.width >= display_dimensions.width {
                 return Ok(Direction::SW);
             }
-
             if focused_node.rect.y + focused_node.rect.height >= display_dimensions.height {
                 return Ok(Direction::NE);
             }
         }
+
         Direction::SW => {
             if focused_node.rect.x <= 0 {
                 return Ok(Direction::SE);
             }
-
             if focused_node.rect.y + focused_node.rect.height >= display_dimensions.height {
                 return Ok(Direction::NW);
             }
         }
+
         Direction::NW => {
             if focused_node.rect.x <= 0 {
                 return Ok(Direction::NE);
             }
-
             if focused_node.rect.y <= 0 {
                 return Ok(Direction::SW);
             }
         }
     }
 
-    if old_direction == Direction::NE {}
-
-    // println!("{:#?}", display_dimensions);
     Ok(old_direction)
 }
 
-fn move_window(ipc: &mut Connection, direction: &Direction) {
+fn move_focused_window(ipc: &mut Connection, direction: &Direction) {
     match direction {
         Direction::NE => {
             _ = ipc.run_command(format!("move up {}", 1));
@@ -107,8 +109,4 @@ fn move_window(ipc: &mut Connection, direction: &Direction) {
             _ = ipc.run_command(format!("move right {}", -1));
         }
     }
-}
-
-fn float_node(ipc: &mut Connection) -> Result<Vec<Result<(), Error>>, Error> {
-    ipc.run_command("floating on")
 }
